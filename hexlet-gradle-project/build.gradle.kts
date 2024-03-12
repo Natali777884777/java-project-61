4dc8e80dc3d76db447fe98f0668b89149b595e8e
Natalia <natali777884@gmail.com>
2024-03-12 17:13:37 +0100

    Compilation and assembly

---
+++ hexlet-gradle-project/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    // Поддержка запуска из командной строки
+    application
+}
+
+repositories {
+    // Подключаем автоматическая работа с репозиторием Maven Central
+    mavenCentral()
+}
+
+// Для плагина application указываем главный класс приложения
+application {
+    // Входная точка
+
+    mainClass.set("io.hexlet.example.App")
+}
